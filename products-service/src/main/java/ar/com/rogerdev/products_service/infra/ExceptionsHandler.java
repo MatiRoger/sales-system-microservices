c4fbcb9fce626d3c7bd4fca745fad7fe8789d518
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-06-10 15:20:38 -0300

    feat: implemented simple exceptions handler

--- products-service/src/main/java/ar/com/rogerdev/products_service/infra/ExceptionsHandler.java
+++ products-service/src/main/java/ar/com/rogerdev/products_service/infra/ExceptionsHandler.java
@@ -1,2 +1,29 @@
-package ar.com.rogerdev.products_service.infra;public class ExceptionsHandler {
+package ar.com.rogerdev.products_service.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404 (EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400 (MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors().stream().map(exceptionResDTO::new);
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private record exceptionResDTO (String field, String message) {
+        public exceptionResDTO (FieldError error) {
+            this( error.getField(), error.getDefaultMessage());
+        }
+    }
+
 }
