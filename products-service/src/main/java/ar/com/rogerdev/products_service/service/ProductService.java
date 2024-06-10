d3d8757f8291f5c534963a5539789bdc0708bec7
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-06-10 15:20:59 -0300

    hotfix: missing annotation in service class

--- products-service/src/main/java/ar/com/rogerdev/products_service/service/ProductService.java
+++ products-service/src/main/java/ar/com/rogerdev/products_service/service/ProductService.java
@@ -6,7 +6,9 @@ import ar.com.rogerdev.products_service.repository.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService implements IProductService{
     @Autowired
     private IProductRepository productRepo;
